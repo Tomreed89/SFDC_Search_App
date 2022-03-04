0aacdb43e92b3fd374dfd89d2b62a0bee4609256
Tomreed89 <39887386+Tomreed89@users.noreply.github.com>
2022-03-04 12:15:57 +0000

    Update 1.3

--- src/App.js
+++ src/App.js
@@ -21,11 +21,23 @@ export default function App() {
   );
 
   function makeRequest() {
-    data = [];
-    alert("Click!");
-    fetch("https://th-apex-http-callout.herokuapp.com/animals")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-    alert(data);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://th-apex-http-callout.herokuapp.com/animals", true);
+    xhr.onload = function (e) {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          //console.log(xhr.responseText);
+          alert("A" + xhr.responseText);
+        } else {
+          //console.error(xhr.statusText);
+          alert("B" + xhr.statusText);
+        }
+      }
+    };
+    xhr.onerror = function (e) {
+      //console.error(xhr.statusText);
+      alert("C" + xhr.statusText);
+    };
+    xhr.send(null);
   }
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
+    Version 1.3
   </StrictMode>,
   rootElement
 );
